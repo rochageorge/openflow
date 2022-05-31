fcd235fe5f62f914d592358083abf2c6c0c65499
George Rocha <35888730+rochageorge@users.noreply.github.com>
2022-05-31 12:20:13 -0300

    Update kb_install.sh

--- kubeflow/kb_install.sh
+++ kubeflow/kb_install.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-sudo su
-snap install microk8s --classic --channel=1.21/stable
-microk8s status
-usermod -a -G microk8s ubuntu
-chown -f -R ubuntu ~/.kube
-
+sudo snap install microk8s --classic --channel=1.21/stable
+sudo usermod -a -G microk8s $USER
+newgrp microk8s
+sudo chown -f -R $USER ~/.kube
 microk8s enable dns storage ingress metallb:10.64.140.43-10.64.140.49
-alias kubectl='microk8s kubectl'
-snap install juju --classic  --channel=2.9/stable
-microk8s config > ~/.kube/config
-juju add-k8s myk8s
-juju bootstrap myk8s myctlr
-juju add-model kubeflow
-juju deploy kubeflow --trust
-
-juju config dex-auth static-username=admin
-juju config dex-auth static-password=mypassword
-
+microk8s status --wait-ready
+sudo snap install juju --classic
+juju bootstrap microk8s
+juju deploy kubeflow-lite --trust
 juju config dex-auth public-url=http://10.64.140.43.nip.io
 juju config oidc-gatekeeper public-url=http://10.64.140.43.nip.io
+juju config dex-auth static-username=admin
+juju config dex-auth static-password=admin
+
